5bab68649bf4d7660bf6cf20bd18d822b167bd1d
Ahmad Addel <a4addel@gmail.com>
2023-01-03 21:28:29 -0800

    delete boilerplate comments and format

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,9 @@
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        BOT_TOKEN: string;
-      }
+  namespace NodeJS {
+    interface ProcessEnv {
+      BOT_TOKEN: string;
     }
   }
-  
-  // If this file has no import/export statements (i.e. is a script)
-  // convert it into a module by adding an empty export statement.
-  export {}
+}
+
+export { }
