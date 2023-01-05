b62ccb49996d3d7249fe149da064388ec0898471
Ahmad Addel <a4addel@gmail.com>
2023-01-05 06:32:33 -0800

    add downloading and merging functionality

--- src/bot.ts
+++ src/bot.ts
@@ -1,33 +1,21 @@
-import { exec } from "child_process";
-import { randomUUID } from "crypto";
 import env from "dotenv";
-import { createWriteStream, fstat, writeFile, writeFileSync } from "fs";
 env.config();
 
-const qualities = {
-  "720p": {
-    "itag": 22,
-  },
-  "360p": {
-    "itag": 18,
-  },
-  "1080p60": {
-    video: {
-      "itag": 399,
-    },
-  },
-}
-
-
-
-
-
+import { randomUUID } from "crypto";
+import { createWriteStream, writeFileSync } from "fs";
+import path from "path";
+import { extension } from "mime-types";
 import { Telegraf } from "telegraf";
 import { Key, Keyboard } from "telegram-keyboard";
+import ytdl, { downloadFromInfo, videoFormat } from "ytdl-core";
+const theWayTo = path.join(
+  process.cwd(),
+  "ffmpeg-n5.1-latest-win64-gpl-5.1/bin/ffmpeg.exe"
+);
 
-import ytdl, { videoFormat } from "ytdl-core";
 import app from "./firebase/init";
-import { getFormatsForStreamsWithAudiosAndVideo, parse } from "./utils/yt-dlp";
+import { parse } from "./utils/yt-dlp";
+import { exec } from "child_process";
 
 const bot = new Telegraf(
   `${process.env.BOT_TOKEN_ID}:${process.env.BOT_TOKEN_SECRET}`
@@ -43,32 +31,30 @@ bot.settings((e) => {
   );
 });
 
+function getBestTrack(
+  formats: Array<videoFormat>,
+  quality: number
+): videoFormat {
+  let fallbackFormat: videoFormat = formats[0];
 
+  loop: for (let index = 0; index < formats.length; index++) {
+    // @ts-ignore
 
-async function handleDownload({
-  url, quality
-}: {
-  url: string, quality: string
-}) {
-  const info = await ytdl.getBasicInfo(url);
-  const qualityDictionary = parse(info.formats);
-
-  const videoUUID = randomUUID();
-  const audioUUID = randomUUID();
-  
-  const videoStream = createWriteStream(videoUUID);
-  const audioStream = createWriteStream(audioUUID);
-
-  
+    // @ts-ignore
+    if (formats[index]?.i <= quality) {
+      fallbackFormat = formats[index];
+      break loop;
+    }
+  }
+  return fallbackFormat;
 }
 
 const YTREGEXP = new RegExp("^(https?://)?((www.)?youtube.com|youtu.be)/.+$");
 bot.hears(YTREGEXP, async (e) => {
   try {
     e.sendChatAction("typing");
+    e.reply("Working on it!");
 
-    
-    
     const qulaity = await app
       .firestore()
       .collection("USERS")
@@ -76,15 +62,78 @@ bot.hears(YTREGEXP, async (e) => {
       .where("id", "==", e.from.id)
       .get();
 
-    const quality = qulaity.docs[0].data().quality || "720p"
-    //   const list = getItagForStreamsWithAudiosAndVideo(info.formats);
+    const quality = qulaity.docs[0].data()?.quality || 720;
 
+    const info = await ytdl.getInfo(e.message.text);
 
-    e.reply(
-      "Wait"
-      // "\nThe Download Functionality is disabled\nThis Bot is for show only to see the code please visit:\nhttps://github.com/a4addel/Youtube-Downloader-Telegram-Bot\n"
+    const parsedFormats = parse(info.formats);
+
+    if (!parsedFormats) return;
+
+    const vid = getBestTrack(parsedFormats.videoTracks, quality);
+    if (!vid) return;
+
+    const videoUUID = randomUUID();
+    const audioUUID = randomUUID();
+
+    const aPath = path.join(
+      process.cwd(),
+      audioUUID + ".ytd"
     );
-  } catch (error) { }
+
+    const vPath = path.join(
+      process.cwd(),
+      videoUUID + ".ytd"
+    );
+    const videoStream = createWriteStream(vPath);
+    const audioStream = createWriteStream(aPath);
+
+    if (vid.hasAudio) {
+      e.reply("Downloading Video");
+      ytdl(e.message.text, { filter: (format) => format.itag === vid.itag })
+        .pipe(videoStream)
+        .on("close", () => {
+          e.sendChatAction("upload_video");
+          e.replyWithVideo({
+            source: vPath,
+            filename: "s",
+          });
+        });
+    } else {
+      e.reply("Downloading Video");
+      ytdl(e.message.text, { filter: (format) => format.itag === vid.itag })
+        .pipe(videoStream)
+        .on("close", () => {
+          e.reply("Downloading Audio");
+          ytdl(e.message.text, {
+            filter: (format) => format.itag === parsedFormats.audioTracks.itag,
+          })
+            .pipe(audioStream)
+            .on("close", () => {
+              const mPath = path.join(process.cwd(), randomUUID() + ".ytd");
+              e.reply("Merging");
+              exec(
+                `ffmpeg.exe -i ${aPath} -i ${vPath} -acodec copy -vcodec copy ${mPath}`
+              ).on("close", () => {
+                e.replyWithVideo({
+                  source: mPath,
+                }).then(() => {
+                  exec(`rm -rf ${aPath} ${vPath} ${mPath}`);
+                });
+              });
+            });
+        });
+    }
+
+    // e.reply(
+    //   "Wait"
+    //   // "\nThe Download Functionality is disabled\nThis Bot is for show only to see the code please visit:\nhttps://github.com/a4addel/Youtube-Downloader-Telegram-Bot\n"
+    // );
+  } catch (error) {
+    console.log(error);
+
+    e.reply("error");
+  }
 });
 
 const not_YT_LINK = new RegExp(
@@ -115,12 +164,29 @@ bot.on("callback_query", (e) => {
       e.answerCbQuery();
       break;
     case "720":
-      e.reply("Code 720p");
-      e.answerCbQuery();
+      app
+        .firestore()
+        .collection("USERS")
+        .doc(e.from?.id + "")
+        .update({
+          quality: 720,
+        })
+        .then(() => {
+          e.answerCbQuery("Done");
+        });
 
       break;
     case "360":
-      e.reply("Code 360p");
+      app
+        .firestore()
+        .collection("USERS")
+        .doc(e.from?.id + "")
+        .update({
+          quality: 360,
+        })
+        .then(() => {
+          e.answerCbQuery("Done");
+        });
       e.answerCbQuery();
       break;
     default:
@@ -139,7 +205,7 @@ bot.start(async (e) => {
   await app.firestore().collection("USERS").add({
     id: e.message.from.id,
     joind: new Date(),
-    quality: "720p",
+    quality: 720,
   });
 
   e.reply("Welcome");
@@ -148,4 +214,6 @@ bot.start(async (e) => {
   ]);
 });
 
-bot.launch();
+bot.launch().then((e) => {
+  console.log("UPP");
+});
--- src/utils/yt-dlp.ts
+++ src/utils/yt-dlp.ts
@@ -1,35 +1,66 @@
-import { videoFormat, audioTrack } from "ytdl-core";
+import ytdl, { videoFormat, audioTrack } from "ytdl-core";
 
 function hasAudio(format: videoFormat) {
-  return format.audioQuality;
+  return format.audioQuality || format.bitrate;
 }
 
 function hasVideo(format: videoFormat) {
   return format.height;
 }
 
-function doesStreamHasAudioAndVideo(format: videoFormat) {
-  return hasAudio(format) && hasVideo(format);
+function isVideo(e: videoFormat) {
+  return !hasAudio(e) && hasVideo(e);
 }
 
-export function getFormatsForStreamsWithAudiosAndVideo(
-  formats: Array<videoFormat>
-) {
-  return formats.filter((format) => doesStreamHasAudioAndVideo(format));
+function isAudio(e: videoFormat) {
+  return hasAudio(e) && !hasVideo(e);
+}
+
+function isTrack(e: videoFormat) {
+  return hasAudio(e) && hasVideo(e);
+}
+
+function trimUnwantedQualities(
+  f: Array<videoFormat>
+): Array<videoFormat> | false {
+  if (!f) return false;
+
+  const maxItag = 399;
+
+  const formats: Array<videoFormat> = [];
+  for (let index = 0; index < f.length; index++) {
+    if (f[index].itag <= maxItag) formats.push(f[index]);
+  }
+
+  const sortedFormats = formats.sort((e, b) => (e.itag > b.itag ? -1 : 1));
+  if (sortedFormats.length === 0) return false;
+  return sortedFormats;
+}
+
+function labelToNumber(label: string) {
+  let num = "";
+  for (let index = 0; index < label.length; index++) {
+    if (Number.isInteger(Number(label[index]))) {
+      num = num + label[index].toString();
+    }
+  }
+
+  return Number(num);
 }
 
 export function parse(formats: Array<videoFormat>) {
-  const list = {
-    audio: [] as Array<videoFormat>,
-    video: [] as Array<videoFormat>,
-    both: [] as Array<videoFormat>,
+  const wantedFormats = trimUnwantedQualities(formats);
+  if (!wantedFormats) return false;
+
+  return {
+    audioTracks: ytdl.filterFormats(wantedFormats, "audioonly")[0],
+    videoTracks: ytdl
+      .filterFormats(wantedFormats, "video")
+      .map((e) => ({ ...e, i: labelToNumber(e.qualityLabel) }))
+      .sort((a, b) => (a.i > b.i ? -1 : 1)),
+    clips: ytdl
+      .filterFormats(wantedFormats, "audioandvideo")
+      .map((e) => ({ ...e, i: labelToNumber(e.qualityLabel) }))
+      .sort((a, b) => (a.i > b.i ? -1 : 1)),
   };
-
-  formats.forEach((e) => {
-    if (hasAudio(e) && hasVideo(e)) list.both.push(e);
-    if (hasAudio(e) && !hasVideo(e)) list.audio.push(e);
-    if (!hasAudio(e) && hasVideo(e)) list.video.push(e);
-  });
-
-  return list;
 }
