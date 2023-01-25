0cdef3bf5b8282817618e0b82778017fff7a6467
Ahmad Addel <a4addel@gmail.com>
2023-01-25 02:59:11 -0800

    refactor yt-dlp utils

--- src/utils/yt-dlp.ts
+++ src/utils/yt-dlp.ts
@@ -1,25 +1,14 @@
-import ytdl, { videoFormat, audioTrack } from "ytdl-core";
+import ytdl, { videoFormat } from "ytdl-core";
 
 
-function labelToNumber(label: string) {
-  let num = "";
-  for (let index = 0; index < label.length; index++) {
-    if (Number.isInteger(Number(label[index]))) {
-      num = num + label[index].toString();
-    }
-  }
-  return Number(num);
-}
+const handleIfIsNumber = (char: any) =>   Number.isInteger(Number(char)) ? char : ""
+const labelToNumber = (label: string)  => Number(label.split("").map(handleIfIsNumber).join(""));
 
 const SortByIndex = (formats: Array<videoFormat & { i: number }>) => formats.sort((a, b) => (a.i > b.i ? -1 : 1))
 const AddIndexToFormat = (formats: Array<videoFormat>) => formats.map((e) => ({ ...e, i: labelToNumber(e.qualityLabel) }))
 
-export function parse(formats: Array<videoFormat>) {
-
-
-  return {
-    audioTracks: ytdl.filterFormats(formats, "audioonly")[0], // Always get the best audios trach possible
-    videoTracks: SortByIndex(AddIndexToFormat(ytdl.filterFormats(formats, "video"))),
-    clips: SortByIndex(AddIndexToFormat(ytdl.filterFormats(formats, "audioandvideo")))
-  };
-}
+export const parse = (formats: Array<videoFormat>) => ({
+  audioTracks: ytdl.filterFormats(formats, "audioonly")[0], // Always get the best audios trach possible
+  videoTracks: SortByIndex(AddIndexToFormat(ytdl.filterFormats(formats, "video"))),
+  clips: SortByIndex(AddIndexToFormat(ytdl.filterFormats(formats, "audioandvideo")))
+})
